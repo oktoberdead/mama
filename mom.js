1b8588116fb86957223f5fe02cbe08738b807f73
h <78501382+oktoberdead@users.noreply.github.com>
2021-02-27 00:01:51 +0300

    Add files via upload

--- mom.js
+++ mom.js
@@ -4,10 +4,13 @@ mama.style.top = (window.innerHeight - 80) / 2 + "px";
 mama.style.transition = "all 1s";
 
 let rot = 5;
+let plus = 5;
 
 function e(){
 mama.style.transform = "rotate("+ rot+ "deg)";
-rot+=5;
+rot += plus;
+if(plus < 100)
+plus += 0.5;
 setTimeout (e, 1);
 }
 
